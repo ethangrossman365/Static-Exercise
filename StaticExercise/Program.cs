e07c1399449a3f2cb3f90f5792de202ef9d7b8ed
ethan grossman <ethangrossman3@gmail.com>
2023-03-14 12:07:53 -0500

    finished exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Fahrenheit to Celsius");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+
+            Console.WriteLine("Celsius to Fahrenheit");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
+
         }
     }
 }
